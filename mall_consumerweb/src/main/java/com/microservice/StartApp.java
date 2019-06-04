7e55f2d3b1dd3504c6f792adeb5bf5e109778946
eatberry <1009452821@qq.com>
2019-06-05 00:22:39 +0800

    test

--- mall_consumerweb/src/main/java/com/microservice/StartApp.java
+++ mall_consumerweb/src/main/java/com/microservice/StartApp.java
@@ -5,6 +5,7 @@ import org.apache.servicecomb.foundation.common.utils.Log4jUtils;
 
 public class StartApp {
     public static void main(String[] args) throws Exception{
+
         Log4jUtils.init();
         BeanUtils.init();
     }
