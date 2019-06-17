ef7be17ce90ead846f0cbd5ca934f842065ffd3d
eatberry <1009452821@qq.com>
2019-06-17 10:02:20 +0800

    test

--- household_providerdeta/src/main/java/com/microservice/provider/HouseHoldProvider.java
+++ household_providerdeta/src/main/java/com/microservice/provider/HouseHoldProvider.java
@@ -2,6 +2,7 @@ package com.microservice.provider;
 
 import com.microservice.model.HouseHold;
 import com.microservice.model.Result;
+
 import com.netflix.config.DynamicPropertyFactory;
 import com.netflix.config.DynamicStringProperty;
 import org.apache.servicecomb.provider.rest.common.RestSchema;
@@ -13,21 +14,18 @@ import org.springframework.web.bind.annotation.RequestMethod;
 
 import java.util.ArrayList;
 import java.util.List;
-
 //生产者
 @RestSchema(schemaId = "household")
 @RequestMapping(path = "/provider/v0")
 public class HouseHoldProvider {
 
-    private HouseHold houseHold1 = new HouseHold("无霜冷藏冰箱", 2500.00, "海尔", "箱门结构: 三门式, 制冷方式: 风冷, 能效等级: 一级, 服务: 并且本店节假日同城开启配送服务，只要活动期间付款成功，均可享受免费配送服务");
+    private HouseHold houseHold1 = new HouseHold("无霜冷藏冰箱", 2500.00, "海尔", "箱门结构: 三门式, 制冷方式: 风冷, 能效等级: 一级, 服务: 本店节假日同城开启配送服务，只要活动期间付款成功，均可享受免费配送服务");
     private HouseHold houseHold2 = new HouseHold("变频空调", 3000.00, "格力", "空调类型: 壁挂式, 最大功率: 大1.5匹, 冷暖类型: 冷暖型");
     private HouseHold houseHold3 = new HouseHold("洗衣机", 1799.99, "合肥美的", "产品类型: 滚筒洗衣机, 使用方式: 全自动, 洗衣程序: 单脱水, 羽绒服, 混合羊毛, 其他");
 
     private DynamicStringProperty sellPrefix = DynamicPropertyFactory.getInstance().getStringProperty("household.sellhousehold","",notifyConfigRefreshed());
 
-
     private static final Logger LOGGER = LoggerFactory.getLogger(HouseHoldProvider.class);
-
 
     private Runnable notifyConfigRefreshed(){
         return () ->LOGGER.info("config[household.sellhousehold] changed to [{}] !",sellPrefix.getValue());
@@ -45,7 +43,7 @@ public class HouseHoldProvider {
         if ("bingxiang".equals(name)){
             list.add(houseHold1);
             return new Result(list);
-        }else if ("空调".equals(name)){
+        }else if ("空调1".equals(name)){
             list.add(houseHold2);
             return new Result(list);
         }else if ("洗衣机".equals(name)){
@@ -74,6 +72,7 @@ public class HouseHoldProvider {
     public List<HouseHold> list(){
         List<HouseHold> houseHolds = new ArrayList<HouseHold>();
         houseHolds.add(houseHold1);
+        System.out.println("");
         houseHolds.add(houseHold2);
         houseHolds.add(houseHold3);
         return houseHolds;
